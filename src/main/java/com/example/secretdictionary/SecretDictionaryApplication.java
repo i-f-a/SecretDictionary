9c37e7c8e81d87dc25d1e24ff255043070f89a3b
Fedor Ivanov <kenola82007@gmail.com>
2023-02-10 19:50:19 +0300

    Initial commit

---
+++ src/main/java/com/example/secretdictionary/SecretDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.secretdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretDictionaryApplication.class, args);
+    }
+
+}
